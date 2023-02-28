013c0c162474bb913072d50ffd14d409f57df0e1
Eko450-eng <ekrem@wipdesign.de>
2023-02-28 13:45:17 +0100

    HotFix

--- app/settings/[user]/push.js
+++ app/settings/[user]/push.js
@@ -7,5 +7,6 @@ const key = {
 
 push.setVapidDetails('mailto:ekrem@wipdesign.de', key.publicKey, key.privateKey)
 
-const sub = {}
+const sub = { user: '386t02u3fas5sfi' }
+
 push.sendNotification(sub, "test")
